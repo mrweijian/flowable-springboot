40452af8fb06f5ddcb59317dcc087d5441de9f81
weijian <1026760118@qq.com>
2022-03-24 16:39:48 +0800

    flowable学习

---
+++ src/main/java/com/wj/demo/MyApplication.java
@@ -0,0 +1,17 @@
+package com.wj.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName MyApplication
+ * @Author weijian
+ * @Date 2022/3/14
+ */
+
+@SpringBootApplication
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
